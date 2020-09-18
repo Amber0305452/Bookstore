89c1b6f1a5bdad91bebe4f932e04990dbc052e8b
Amber <44774806+Amber0305452@users.noreply.github.com>
2020-09-18 12:57:48 +0200

    done i think

--- BookstoreApplication/BookstoreApplication/Controllers/AccountController.cs
+++ BookstoreApplication/BookstoreApplication/Controllers/AccountController.cs
@@ -136,7 +136,7 @@ namespace BookstoreApplication.Controllers
 
         //
         // GET: /Account/Register
-        [AllowAnonymous]
+        [Authorize(Roles = "SuperAdmin")]
         public ActionResult Register()
         {
             return View();
@@ -145,7 +145,7 @@ namespace BookstoreApplication.Controllers
         //
         // POST: /Account/Register
         [HttpPost]
-        [AllowAnonymous]
+        [Authorize(Roles = "SuperAdmin")]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Register(RegisterViewModel model)
         {
